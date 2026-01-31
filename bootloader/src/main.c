4634659bf3d39410929bb0a87679c81dda100eb2
Алексей Галов <jifaxin@niuitmo.ru>
2026-01-31 13:30:43 +0300

    вытаскивание значений регистров gcc

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -59,6 +59,53 @@ void edl_reboot() {
     /* log(LOG_INFO, "edl_reboot(): MPM2_MPM_PS_HOLD done"); */
 }
 
+#define GCC_BASE 0x1800000
+
+#define TLMM_BASE 0x1000000
+#define GPIO_CTL(n) (TLMM_BASE + (n) * 0x1000 + 0)
+#define GPIO_IO(n) (TLMM_BASE + (n) * 0x1000 + 4)
+
+// lk2nd
+/* GPIO TLMM: Direction */
+#define GPIO_INPUT      0
+#define GPIO_OUTPUT     1
+
+/* GPIO TLMM: Pullup/Pulldown */
+#define GPIO_NO_PULL    0
+#define GPIO_PULL_DOWN  1
+#define GPIO_KEEPER     2
+#define GPIO_PULL_UP    3
+
+/* GPIO TLMM: Drive Strength */
+#define GPIO_2MA        0
+#define GPIO_4MA        1
+#define GPIO_6MA        2
+#define GPIO_8MA        3
+#define GPIO_10MA       4
+#define GPIO_12MA       5
+#define GPIO_14MA       6
+#define GPIO_16MA       7
+
+/* GPIO TLMM: Status */
+#define GPIO_ENABLE     0
+#define GPIO_DISABLE    1
+
+/* GPIO_IN_OUT register shifts. */
+#define GPIO_IN         BIT(0)
+#define GPIO_OUT        BIT(1)
+
+
+#define CLK_CTL_BASE                       0x1800000
+#define GPLL0_STATUS                       (CLK_CTL_BASE + 0x2101C)
+#define GPLL2_STATUS                       (CLK_CTL_BASE + 0x4A01C)
+#define GPLL0_MODE                         (CLK_CTL_BASE + 0x21000)
+#define APCS_GPLL_ENA_VOTE                 (CLK_CTL_BASE + 0x45000)
+#define APCS_CLOCK_BRANCH_ENA_VOTE         (CLK_CTL_BASE + 0x45004)
+#define GPLL4_MODE                         (CLK_CTL_BASE + 0x24000)
+#define GPLL4_STATUS                       (CLK_CTL_BASE + 0x24024)
+#define GPLL6_STATUS                       (CLK_CTL_BASE + 0x3701C)
+// ------------
+
 void main() {
     clear_debug_buffer();
 
@@ -67,6 +114,15 @@ void main() {
     u64 current_el;
     asm("mrs %0, CurrentEL" : "=r"(current_el));
     logf(LOG_INFO, "current EL: %z", current_el >> 2);
+
+    logf(LOG_INFO, "GPLL0_STATUS: %x", readu32(GPLL0_STATUS));
+    logf(LOG_INFO, "GPLL2_STATUS: %x", readu32(GPLL2_STATUS));
+    logf(LOG_INFO, "GPLL0_MODE: %x", readu32(GPLL0_MODE));
+    logf(LOG_INFO, "APCS_GPLL_ENA_VOTE: %x", readu32(APCS_GPLL_ENA_VOTE));
+    logf(LOG_INFO, "APCS_CLOCK_BRANCH_ENA_VOTE: %x", readu32(APCS_CLOCK_BRANCH_ENA_VOTE));
+    logf(LOG_INFO, "GPLL4_MODE: %x", readu32(GPLL4_MODE));
+    logf(LOG_INFO, "GPLL4_STATUS: %x", readu32(GPLL4_STATUS));
+    logf(LOG_INFO, "GPLL6_STATUS: %x", readu32(GPLL6_STATUS));
 
     edl_reboot();
 
