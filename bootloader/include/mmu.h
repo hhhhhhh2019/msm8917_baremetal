9322bd45d8b8265fea204bf781696d1ed5a6405b
Алексей <agalove543210@gmail.com>
2026-07-15 01:05:35 +0300

    сделал все сам, но результат тот же

--- bootloader/include/mmu.h
+++ bootloader/include/mmu.h
@@ -1,10 +1,6 @@
 #ifndef MMU_H_
 #define MMU_H_
 
-#include "stdint.h"
-
-typedef u64 tlb_table_t[8192] __attribute__((aligned(65536)));
-
 void mmu_init();
 
 #endif // MMU_H_
--- bootloader/src/mmu.c
+++ bootloader/src/mmu.c
@@ -1,127 +1,72 @@
 #include "mmu.h"
 #include "fb.h"
 
-tlb_table_t tlb_table_kernel = {0};
+u64 tlb_kernel_table[8192] __attribute__((aligned(65536)));
 
-#define PTE_VALID          (1ULL << 0)
-#define PTE_BLOCK          (0ULL << 1)
-#define PTE_TABLE          (1ULL << 1)
-#define PTE_AF             (1ULL << 10)
-#define PTE_INNER_SH       (3ULL << 8)
+/* если верить gemini размер таблицы = размеру страницы (= 64КБ в моем случае)
+ * 64*1024/8 = 8192 индеска в таблице -> 13 бит на индекс
+ * TxSZ = 24 -> размер вирт. адреса = 40 бит
+ * смещение на странице - 16 бит(64КБ)
+ * индекс - 13 бит
+ * 40 = 16 + 13 * 2
+ * -> имеем два уровня таблиц, где самая верхная мапит сразу 512 МБ(64КБ * 8192)
+ */
 
-#define ATTR_INDEX_DEVICE  0
-#define ATTR_INDEX_NORMAL  1
-
-#define MMIO_FLAGS         (PTE_VALID | PTE_BLOCK | (ATTR_INDEX_DEVICE << 2) | PTE_AF)
-#define RAM_FLAGS          (PTE_VALID | PTE_BLOCK | (ATTR_INDEX_NORMAL << 2) | PTE_AF | PTE_INNER_SH)
-
-__attribute__((noinline))
-void loop();
+#define MMIO_FLAGS (0b01 << 0) | (1 << 10) | (0 << 2)
+#define RAM_FLAGS  (0b01 << 0) | (1 << 10) | (1 << 2)
 
 void mmu_init() {
-    tlb_table_kernel[0] = 0x00000000ULL | MMIO_FLAGS;
-    tlb_table_kernel[1] = 0x20000000ULL | MMIO_FLAGS;
-    tlb_table_kernel[2] = 0x40000000ULL | MMIO_FLAGS;
-    tlb_table_kernel[3] = 0x60000000ULL | MMIO_FLAGS;
-
-    tlb_table_kernel[4] = 0x80000000ULL | RAM_FLAGS;
-    tlb_table_kernel[5] = 0xA0000000ULL | RAM_FLAGS;
-    tlb_table_kernel[6] = 0xC0000000ULL | RAM_FLAGS;
-    tlb_table_kernel[7] = 0x80000000ULL | RAM_FLAGS;
-
-    asm volatile("dsb sy" ::: "memory");
-
     fb_put_char('1');
 
-    // attr0 = 0b00000000 device memory, nGnRnE
-    // attr1 = 0b11111111 normal memory, Inner Write-back non-transient
-    asm volatile("msr MAIR_EL1, %0" :: "r"(0xff00));
+    u8 attr0 = (0b0000 << 0) | (0b0000 << 4); // Device memory, Device-nGnRnE memory
+    u8 attr1 = (0b0100 << 0) | (0b0100 << 4); // Normal Memory, Outer Non-Cacheable, Normal memory, Inner Non-Cacheable
+    asm volatile("msr MAIR_EL1, %0" :: "r"(attr0 | (attr1 << 8)));
 
     fb_put_char('2');
 
-    // [36] = 1 16 bit ASID
-    // [34:32] = 000 Intermediate Physical Address Size 32 bits, 4GB
-    // [31:30] = 11 TTBR1_EL1 granule size 64KB
-    // [29:28] = 10 TTBR1_EL1 Outer shareable
-    // [27:26] = 01 TTBR1_EL1 Outer Write-Back Write-Allocate Cacheable
-    // [25:24] = 01 TTBR1_EL1 Inner Write-Back Write-Allocate Cacheable.
-    // [23] = 0 Perform translation table walk using TTBR1_EL1
-    // [22] = 0 TTBR0_EL1.ASID defines the ASID
-    // [21:16] = 011000 Size offset of the memory region addressed by TTBR1_EL1.
-    // [15:14] = 01 TTBR0_EL1 granule size = 64KB
-    // [13:12] = 10 TTBT_EL1 Outer shareable
-    // [11:10] = 01 TTBR0_EL1 Outer Write-Back Write-Allocate Cacheable
-    // [9:8] = 01 TTBR0_EL1 Inner Write-Back Write-Allocate Cacheable.
-    // [7] = 0 Perform translation table walk using TTBR0_EL1
-    // [5:0] = 011000 Size offset of the memory region addressed by TTBR0_EL1.
-    asm volatile("msr TCR_EL1, %0" :: "r"(0b1000011100101000110000110010100011000));
+    tlb_kernel_table[0] = 0x00000000 | MMIO_FLAGS;
+    tlb_kernel_table[1] = 0x20000000 | MMIO_FLAGS;
+    tlb_kernel_table[2] = 0x40000000 | MMIO_FLAGS;
+    tlb_kernel_table[3] = 0x60000000 | MMIO_FLAGS;
+
+    tlb_kernel_table[4] = 0x80000000 | RAM_FLAGS;
+    tlb_kernel_table[5] = 0xa0000000 | RAM_FLAGS;
+    tlb_kernel_table[6] = 0xc0000000 | RAM_FLAGS;
+    tlb_kernel_table[7] = 0xe0000000 | RAM_FLAGS;
+
+    asm volatile("msr TTBR0_EL1, %0" :: "r"(tlb_kernel_table));
+    asm volatile("msr TTBR1_EL1, %0" :: "r"(tlb_kernel_table));
 
     fb_put_char('3');
 
-    asm volatile("msr TTBR0_EL1, %0" :: "r"(tlb_table_kernel));
-    asm volatile("msr TTBR1_EL1, %0" :: "r"(tlb_table_kernel));
+    u64 tcr = (24ULL << 0) |    // T0SZ
+              (0b00ULL << 8) |  // Inner non cachable
+              (0b00ULL << 10) | // Outer non cachable
+              (0b00ULL << 12) | // Non shareable
+              (0b01ULL << 14) | // TTBR0 granule size = 64KB
+              (24ULL << 16) | // T1ZS
+              (0b00ULL << 24) | // Inner non cachable
+              (0b00ULL << 26) | // Outer non cachable
+              (0b00ULL << 28) | // Non shareable
+              (0b11ULL << 30) | // TTBR1 granule size = 64KB
+              (0b000ULL << 32) | // 4GB IPS
+              (1ULL << 36);  // 16bit ASID
+
+    asm volatile("msr TCR_EL1, %0" :: "r"(tcr));
 
     fb_put_char('4');
 
-    asm volatile(
-        "ic ialluis     \n"
-        "dsb sy         \n"
-        "tlbi vmalle1is \n"
-        "dsb sy         \n"
-        "isb            \n"
-    );
+    asm volatile("isb\ndsb sy");
 
-    fb_put_char('5');
+    // enable MMU
+    u64 scr;
+    asm volatile("mrs %0, SCTLR_EL1" : "=r"(scr));
+    scr |= (1 <<  0) | // mmu
+           (0 <<  2) | // data cache
+           (0 << 12);  // instruction cache
+    asm volatile("msr SCTLR_EL1, %0" :: "r"(scr));
 
-    u64 sctrl;
+    asm volatile("isb \ndsb sy");
 
-    asm volatile("mrs %0, SCTLR_EL1" : "=r"(sctrl));
-
-    fb_put_char('6');
-
-    sctrl |= (1 <<  0) | // MMU
-             (0 <<  2) | // Cache
-             (0 << 12);  // ICache
-
-    sctrl &= ~(1 << 28);
-    sctrl &= ~(1 << 29);
-
-    fb_put_char('7');
-
-    asm volatile("msr SCTLR_EL1, %0\n"
-                 "isb"
-                 :: "r"(sctrl));
-
-    fb_put_char('8');
-
-    u64 current_sp;
-    asm volatile("mov %0, sp" : "=r"(current_sp));
-
-    asm volatile("dc ivac, %0" :: "r"(current_sp));
-    asm volatile("dc ivac, %0" :: "r"(current_sp + 64));
-    asm volatile("dc ivac, %0" :: "r"(current_sp - 64));
-
-    asm volatile("dsb sy\n"
-                 "isb");
-
-    loop();
-
-    for (int a = 0; a < 10; a++) {
-        fb_put_char('a');
-        for (volatile u32 i = 0; i < 1000000; i++);
-    }
-}
-
-__attribute__((noinline))
-void loop() {
-    u8* fb = (u8*)(0x90001000ULL - 0x80000000ULL + 0xE0000000ULL);
-
-    for (u32 x = 0; x < 100; x++) {
-        for (u32 y = 0; y < 100; y++) {
-            u32 offset = (x + y * 720) * 3;
-            fb[offset + 0] = 100;
-            fb[offset + 1] = 200;
-            fb[offset + 2] = 250;
-        }
-    }
+    fb_put_char('f');
 }
