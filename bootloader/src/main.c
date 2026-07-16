288fc20c1fc391b1b8b2eec35a9f345df138ea9c
Алексей <agalove543210@gmail.com>
2026-07-16 16:23:37 +0300

    починил перезапуск после 6 секунд работы

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -77,13 +77,29 @@ void edl_reboot() {
 }
 
 #define MMIO_FLAGS (0b01 << 0) | (1 << 10) | (0 << 2)
-#define RAM_FLAGS  (0b01 << 0) | (1 << 10) | (1 << 2)
+#define RAM_FLAGS  (0b01 << 0) | (1 << 10) | (1 << 2) | (0b11 << 8)
+
+/* если верить gemini размер таблицы = размеру страницы (= 64КБ в моем случае)
+ * 64*1024/8 = 8192 индеска в таблице -> 13 бит на индекс
+ * TxSZ = 24 -> размер вирт. адреса = 40 бит
+ * смещение на странице - 16 бит(64КБ)
+ * индекс - 13 бит
+ * 40 = 16 + 13 * 2
+ * -> имеем два уровня таблиц, где самая верхная мапит сразу 512 МБ(64КБ * 8192)
+ */
 
 u64 tlb_kernel_table[8192] __attribute__((aligned(65536)));
 
+void timer_handler(u32 irq, struct registers *regs) {
+    fb_put_char('t');
+    fb_flush();
+
+    start_timer(100);
+}
+
 void main() {
     u8 attr0 = (0b0000 << 0) | (0b0000 << 4); // Device memory | Device-nGnRnE memory
-    u8 attr1 = (0b0111 << 0) | (0b0111 << 4); // Normal Memory, Outer Write-back transient | Normal Memory, Inner Write-back transient
+    u8 attr1 = (0b1111 << 0) | (0b1111 << 4); // Normal Memory, Outer Write-back Non-transient | Normal Memory, Inner Write-back Non-transient
     asm volatile("msr MAIR_EL1, %0" :: "r"(attr0 | (attr1 << 8)));
 
     tlb_kernel_table[0] = 0x00000000 | MMIO_FLAGS;
@@ -94,20 +110,20 @@ void main() {
     tlb_kernel_table[4] = 0x80000000 | RAM_FLAGS;
     tlb_kernel_table[5] = 0xa0000000 | RAM_FLAGS;
     tlb_kernel_table[6] = 0xc0000000 | RAM_FLAGS;
-    tlb_kernel_table[7] = 0x90000000 | RAM_FLAGS;
+    tlb_kernel_table[7] = 0xe0000000 | RAM_FLAGS;
 
     asm volatile("msr TTBR0_EL1, %0" :: "r"(tlb_kernel_table));
     asm volatile("msr TTBR1_EL1, %0" :: "r"(tlb_kernel_table));
 
     u64 tcr = (24ULL << 0) |    // T0SZ
-              (0b00ULL << 8) |  // Inner non cachable
-              (0b00ULL << 10) | // Outer non cachable
-              (0b00ULL << 12) | // Non shareable
+              (0b01ULL << 8) |  // Inner Write-Back Write-Allocate Cacheable
+              (0b01ULL << 10) | // Outer Write-Back Write-Allocate Cacheable
+              (0b11ULL << 12) | // Inner shareable
               (0b01ULL << 14) | // TTBR0 granule size = 64KB
               (24ULL << 16) | // T1ZS
-              (0b00ULL << 24) | // Inner non cachable
-              (0b00ULL << 26) | // Outer non cachable
-              (0b00ULL << 28) | // Non shareable
+              (0b01ULL << 24) | // Inner Write-Back Write-Allocate Cacheable
+              (0b01ULL << 26) | // Outer Write-Back Write-Allocate Cacheable
+              (0b11ULL << 28) | // Inner shareable
               (0b11ULL << 30) | // TTBR1 granule size = 64KB
               (0b000ULL << 32) | // 4GB IPS
               (1ULL << 36);  // 16bit ASID
@@ -122,6 +138,11 @@ void main() {
         "isb            \n"
     );
 
+    for (u64 i = 0; i < sizeof(tlb_kernel_table); i += 64) {
+        asm volatile("dc civac, %0" :: "r"(((u64)tlb_kernel_table) + i));
+    }
+    asm volatile("dsb sy\nisb" ::: "memory");
+
     // enable MMU
     u64 scr;
     asm volatile("mrs %0, SCTLR_EL1" : "=r"(scr));
@@ -130,13 +151,19 @@ void main() {
            (1 << 12);  // instruction cache
     asm volatile("msr SCTLR_EL1, %0" :: "r"(scr));
 
-    asm volatile("isb \ndsb sy");
+    asm volatile(
+        "ic ialluis     \n"
+        "dsb sy         \n"
+        "tlbi vmalle1is \n"
+        "dsb sy         \n"
+        "isb            \n"
+    );
 
-    fb_init();
-    fb_init_addres((void*)0x90001000);
+    set_vector_table(&vector_table);
+    gic_init();
 
-    /* set_vector_table(&vector_table); */
-    /* gic_init(); */
+    gic_unmask_interrupt(19);
+    set_irq_handler(289, &timer_handler);
 
     /*
      D or bit 9 - when it’s set to 1, debug exceptions are masked;
@@ -145,7 +172,14 @@ void main() {
      F or bit 6 - when it’s set to 1, FIQs are masked.
      */
     /* asm volatile("msr daifset, #15" ::: "memory"); */
-    /* asm volatile("msr daifclr, #15" ::: "memory"); */
+    asm volatile("msr daifclr, #15" ::: "memory");
+
+    fb_init();
+    fb_init_addres((void*)0x90001000);
+
+    start_timer(1);
+
+    while (1) { asm volatile("wfi"); }
 
     log(LOG_INFO, "reboot");
 
